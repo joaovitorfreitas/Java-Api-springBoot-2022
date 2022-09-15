75ca4426536fd31b3c9ac6398fd70eb6849b61b0
joaovitorfreitas <joaovdlf@gmail.com>
2022-09-15 12:21:08 -0300

    Feature: Adicionando API Java

---
+++ UserAPi/src/main/java/br/com/springboot/UserAPi/UserAPiApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.UserAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAPiApplication.class, args);
+	}
+
+}
---
+++ UserAPi/src/test/java/br/com/springboot/UserAPi/UserAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.UserAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
