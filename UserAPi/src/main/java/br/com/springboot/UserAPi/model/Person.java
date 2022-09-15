18d2109d01cc94eb3d343c82ead3b73506fc57d3
joaovitorfreitas <joaovdlf@gmail.com>
2022-09-15 15:57:41 -0300

    Feature: Adição do model de pessoas

---
+++ UserAPi/src/main/java/br/com/springboot/UserAPi/model/Person.java
@@ -0,0 +1,31 @@
+package br.com.springboot.UserAPi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private Long id;
+
+    private String Cpf;
+
+    private String Street;
+
+    private Integer numberHome;
+
+    private String District;
+
+    private String Complement;
+
+    private String County;
+
+    private String Uf;
+
+    private String Rg;
+}
